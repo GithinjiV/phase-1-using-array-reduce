e1880be1a95d2fa52a443058af7398318ceb4122
vinnie <githinjigicheru146@gmail.com>
2024-02-07 11:04:06 +0300

    Used the reduce method to get the sum of elements in an array

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((sum, batteries)=> batteries + sum , 0)
+
+// incomingStudents.reduce(function(showcases, student) { showcases[studentSorter.showcaseAssign(student)].push(student)}, allShowcases)
+
+
+// incomingStudents.reduce(
+//     (showcases, student)=>showcases[studentSorter.showcaseAssign(student)].push(student),allShowcases);
